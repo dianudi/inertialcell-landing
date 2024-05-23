d4199c3ad78e7a31a60b8379d2b034d3a77a7b37
dianudi <144350056+dianudi@users.noreply.github.com>
2024-05-23 22:34:45 +0700

    add location map and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
+import "leaflet/dist/leaflet.css";
+import aos from "aos";
+import { useEffect } from "react";
 
 export default function App() {
   const routers = createBrowserRouter([{ path: "/", element: <Home /> }]);
+  useEffect(() => {
+    aos.init();
+  }, []);
+
   return (
     <>
       <RouterProvider router={routers} />
--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-
 export default function TopBar() {
   return (
     <div>
-      <div className="navbar justify-between bg-indigo-600">
+      <div className="navbar justify-between bg-transparent">
         <div className="flex container mx-auto">
           <a className="btn btn-ghost text-3xl relative">
             <span className="text-purple-200">Inertia</span>
             <span className="text-pink-600">Cell</span>
-            <i className="bi bi-gear text-lg animate-pulse text-red-500 absolute top-0 -right-1"></i>
+            <i className="bi bi-tools text-lg animate-pulse text-red-500 absolute top-0 -right-1"></i>
           </a>
-          <ul className="ms-20 flex gap-5">
+          <ul className="ms-20 hidden lg:flex gap-5 ">
             <li>
-              <a className="text-white" href="">
+              <a className="text-red-500" href="">
                 Beranda
               </a>
             </li>
@@ -34,7 +32,7 @@ export default function TopBar() {
           </ul>
         </div>
         <div>
-          <button className="block text-white text-4xl bi bi-list"></button>
+          <button className="block md:hidden text-white text-4xl bi bi-list"></button>
         </div>
       </div>
     </div>
